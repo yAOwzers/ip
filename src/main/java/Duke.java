c231b68086f46d88069047abf45303bcb6ecb2ef
yAOwzers <yAOwzers@users.noreply.github.com>
2020-08-27 14:48:48 +0800

    add Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,16 @@
 public class Duke {
+
+    public static void greet() {
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+    }
+
+    public static void lineSeparator() {
+        System.out.println("_________________________");
+    }
+
+    public static void goodbye() {
+        System.out.println("Bye. Hope to see you again soon!");
+    }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +18,12 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        lineSeparator();
+        greet();
+        lineSeparator();
+        goodbye();
+        lineSeparator();
     }
+
+
 }
