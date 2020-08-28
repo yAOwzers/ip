58d6b4d677d2e870b853ef089ff334c2b91dd80f
yAOwzers <yAOwzers@users.noreply.github.com>
2020-08-28 11:08:43 +0800

    add Task File

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Task {
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
+    }
+
+    public void printTask() {
+        String symbol = getStatusIcon();
+        System.out.println("[" + symbol + "]" + this.description);
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
 }
+
+
