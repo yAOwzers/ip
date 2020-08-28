936735ae976b7d1c6196e08801a0c927d958a2ad
yAOwzers <yAOwzers@users.noreply.github.com>
2020-08-28 11:04:32 +0800

    Level 3: Mark as done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -11,11 +11,10 @@ public class Duke {
         System.out.println("_________________________");
     }
 
-
-
     public static void goodbye() {
         System.out.println("Bye. Hope to see you again soon!");
     }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -31,27 +30,51 @@ public class Duke {
 
         String[] storeUserText = new String[100];
         int count = 0;
+        Task[] taskList = new Task[100];
 
-        while(true) {
+        while (true) {
             Scanner input = new Scanner(System.in);
-
             String echo = input.nextLine();
-            if(echo.equals("bye")){
+
+            //exit
+            if (echo.equals("bye")) {
+                goodbye();
                 break;
             }
 
-            storeUserText[count] = echo;
-            lineSeparator();
-            System.out.println("added: " + echo);
-            lineSeparator();
-            count++;
+            //List
+            else if (echo.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
+                for (int i = 0; i < count; i++) {
+                    System.out.print((i + 1) + ". ");
+                    taskList[i].printTask();
+                }
+            } else {
+                //check for done command
+                String[] checkDone = echo.split(" ");
+                if (checkDone[0].equals("done")) {
+                    //check if it is a valid input eg. "done 2" and NOT "done"
+                    if (checkDone.length < 2) {
+                        System.out.println("Invalid input!");
+                    }
+
+                    int indexOfDoneTask = Integer.parseInt(checkDone[1]) - 1;
+                    taskList[indexOfDoneTask].markAsDone();
+                    System.out.print("Nice! I've marked this task as done:\n");
+                    taskList[indexOfDoneTask].printTask();
+                } else {
+                    storeUserText[count] = echo;
+
+                    //user reply
+                    lineSeparator();
+                    System.out.println("added: " + echo);
+                    lineSeparator();
+
+                    Task addTask = new Task(echo);
+                    taskList[count] = addTask;
+                    count++;
+                }
+            }
         }
-
-
-        goodbye();
-
-
     }
-
-
 }
